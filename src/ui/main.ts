dfc214f79f2a2bb4d218420396238dfd2a9eb9bd
Marc Dingena <marc.dingena@gmail.com>
2021-04-13 22:42:34 +0800

    Ship VoodooListener executable with Electron

--- src/ui/main.ts
+++ src/ui/main.ts
@@ -1,9 +1,11 @@
+import path from 'path';
+import { execFile } from 'child_process';
 import { app, BrowserWindow } from 'electron';
 import isDev from 'electron-is-dev';
 
 let mainWindow: BrowserWindow | null = null;
 
-const createWindow = (): void => {
+const createWindow = async (): Promise<void> => {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
@@ -12,8 +14,17 @@ const createWindow = (): void => {
     }
   });
   console.log({ isDev });
-  mainWindow.loadURL(isDev ? 'http://localhost:9000' : `file://${__dirname}/../../build/ui/index.html`);
+  await mainWindow.loadURL(isDev ? 'http://localhost:9000' : `file://${__dirname}/../../build/ui/index.html`);
   mainWindow.on('closed', () => (mainWindow = null));
+
+  const exePath = isDev
+    ? path.join(__dirname, '../../build/speech/VoodooListener.exe')
+    : path.join(process.resourcesPath, 'speech/VoodooListener.exe');
+
+  const speech = execFile(exePath);
+  speech.on('exit', exitCode => console.log({ exitCode }));
+  speech.stdout?.on('data', data => console.log({ data }));
+  speech.stderr?.on('data', error => console.error({ error }));
 };
 
 app.on('ready', createWindow);
