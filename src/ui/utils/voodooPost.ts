fbefefb069a432aa9fd481161f8087986dc6bbfb
Marc Dingena <marc.dingena@gmail.com>
2021-05-06 22:22:46 +0800

    Create voodooPost.ts

---
+++ src/ui/utils/voodooPost.ts
@@ -0,0 +1,17 @@
+import fetch from 'electron-fetch';
+
+export const voodooPost = async (accessToken: string, endpoint: string, data: any): Promise<any> => {
+  try {
+    const response = await fetch(endpoint, {
+      method: 'POST',
+      headers: { Authorization: `Bearer ${accessToken}` },
+      body: JSON.stringify(data)
+    });
+
+    if (response.status !== 200 || !response.ok) throw Error(response.statusText);
+
+    return await response.json();
+  } catch (error) {
+    return { ok: false, error: error.message };
+  }
+};
