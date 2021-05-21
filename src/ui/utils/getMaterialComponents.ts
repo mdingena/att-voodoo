3697b5ecb54c737d53f491e7e106d9ee53df41bb
Marc Dingena <marc.dingena@gmail.com>
2021-05-21 21:53:04 +0800

    Map source grammar to spell materials

--- src/ui/utils/getMaterialComponents.ts
+++ src/ui/utils/getMaterialComponents.ts
@@ -1,5 +1,257 @@
+const _isOptional = 0;
+const AppleCoreBurnt = 50484;
+const AppleCoreCooked = 15500;
+const AppleCoreRipe = 902;
+const AppleCoreUnripe = 31614;
+const AppleFullBurnt = 62102;
+const AppleFullCooked = 14844;
+const AppleFullRipe = 40010;
+const AppleFullUnripe = 6740;
+const AppleHalfBurnt = 65526;
+const AppleHalfCooked = 2000;
+const AppleHalfRipe = 7278;
+const AppleHalfUnripe = 62012;
+const BabuLegBone = 32526;
+const BabuLegFullBurnt = 64844;
+const BabuLegFullCooked = 42916;
+const BabuLegFullRipe = 2562;
+const BabuLegHalfBurnt = 50328;
+const BabuLegHalfCooked = 50326;
+const BabuLegHalfRipe = 50252;
+const BlueberryFullBurnt = 49890;
+const BlueberryFullCooked = 47468;
+const BlueberryFullRipe = 45012;
+const BlueberryFullUnripe = 42556;
+const CarrotFullBurnt = 62634;
+const CarrotFullCooked = 60742;
+const CarrotFullRipe = 58838;
+const CarrotFullUnripe = 56934;
+const CarrotHalfBurnt = 64146;
+const CarrotHalfCooked = 63758;
+const CarrotHalfRipe = 63354;
+const CarrotHalfUnripe = 62950;
+const CarrotLeaves = 10440;
+const CarsiIngot = 13158;
+const Coal = 19658;
+const CopperIngot = 32850;
+const CrystalGemBlue = 8002;
+const CrystalShardBlue = 7092;
+const DaisMeatFullBurnt = 41870;
+const DaisMeatFullCooked = 43452;
+const DaisMeatFullRipe = 47394;
+const DaisMeatHalfBurnt = 43674;
+const DaisMeatHalfCooked = 43502;
+const DaisMeatHalfRipe = 43294;
+const Dynamite = 31326;
+const EggplantFullBurnt = 55030;
+const EggplantFullCooked = 54506;
+const EggplantFullRipe = 53982;
+const EggplantFullUnripe = 53458;
+const EggplantHalfBurnt = 62546;
+const EggplantHalfCooked = 62142;
+const EggplantHalfRipe = 61738;
+const EggplantHalfUnripe = 61334;
+const EvinonSteelIngot = 32198;
+const ExplosiveSpike = 50412;
+const Firework = 40748;
+const GarlicFullBurnt = 52934;
+const GarlicFullCooked = 52530;
+const GarlicFullRipe = 52126;
+const GarlicFullUnripe = 49162;
+const GarlicHalfBurnt = 60930;
+const GarlicHalfCooked = 60526;
+const GarlicHalfRipe = 60122;
+const GarlicHalfUnripe = 59718;
+const GarlicLeaves = 1266;
+const GarlicRoots = 57564;
+const GoldIngot = 30092;
+const GoteraSeedlingOrb = 59342;
+const IronIngot = 23012;
+const MushroomBrownFullBurnt = 47126;
+const MushroomBrownFullCooked = 46510;
+const MushroomBrownFullRipe = 61666;
+const MushroomBrownHalfBurnt = 53372;
+const MushroomBrownHalfCooked = 50916;
+const MushroomBrownHalfRipe = 48460;
+const MushroomCaveLargeHalfBurnt = 60330;
+const MushroomCaveLargeHalfCooked = 9414;
+const MushroomCaveLargeHalfRipe = 54182;
+const MushroomCaveSmallFullBurnt = 29148;
+const MushroomCaveSmallFullCooked = 40066;
+const MushroomCaveSmallFullRipe = 58270;
+const MushroomCaveSmallHalfBurnt = 47142;
+const MushroomCaveSmallHalfCooked = 44488;
+const MushroomCaveSmallHalfRipe = 41834;
+const MushroomGlowingFullBurnt = 43856;
+const MushroomGlowingFullCooked = 41428;
+const MushroomGlowingFullRipe = 38966;
+const MushroomGlowingHalfBurnt = 36504;
+const MushroomGlowingHalfCooked = 34042;
+const MushroomGlowingHalfRipe = 31580;
+const MushroomRedFullBurnt = 46660;
+const MushroomRedFullCooked = 46888;
+const MushroomRedFullRipe = 61658;
+const MushroomRedHalfBurnt = 43272;
+const MushroomRedHalfCooked = 40816;
+const MushroomRedHalfRipe = 38360;
+const OnionFullBurnt = 48718;
+const OnionFullCooked = 48214;
+const OnionFullRipe = 47730;
+const OnionFullUnripe = 43966;
+const OnionHalfBurnt = 59314;
+const OnionHalfCooked = 58910;
+const OnionHalfRipe = 58506;
+const OnionHalfUnripe = 58102;
+const OnionLeaves = 29078;
+const OnionRoots = 32068;
+const OrchiIngot = 16464;
+const PhantomGuard = 6940;
+const PotatoFullBurnt = 1574;
+const PotatoFullCooked = 57840;
+const PotatoFullRipe = 54692;
+const PotatoFullUnripe = 52146;
+const PotatoHalfBurnt = 59748;
+const PotatoHalfCooked = 50612;
+const PotatoHalfRipe = 48134;
+const PotatoHalfUnripe = 45822;
+const PotatoSapling = 50692;
+const PotionMedium = 23644;
+const PumpkinFullBurnt = 5532;
+const PumpkinFullCooked = 5010;
+const PumpkinFullRipe = 4488;
+const PumpkinFullUnripe = 3966;
+const PumpkinHalfBurnt = 3444;
+const PumpkinHalfCooked = 3082;
+const PumpkinHalfRipe = 2720;
+const PumpkinHalfUnripe = 2358;
+const PumpkinPieceBurnt = 42218;
+const PumpkinPieceCooked = 41930;
+const PumpkinPieceRipe = 41608;
+const PumpkinPieceUnripe = 41286;
+const RedIronIngot = 31034;
+const RedwoodGoteraCore = 41402;
+const Salt = 49578;
+const SandstoneStone = 55930;
+const SilverIngot = 23528;
+const SoftFabricMediumRoll = 47760;
+const SoftFabricMediumStrips = 63204;
+const SpriggullDrumstickBone = 24406;
+const SpriggullDrumstickFullBurnt = 20570;
+const SpriggullDrumstickFullCooked = 33190;
+const SpriggullDrumstickFullRipe = 43430;
+const SpriggullDrumstickHalfBurnt = 10908;
+const SpriggullDrumstickHalfCooked = 8440;
+const SpriggullDrumstickHalfRipe = 5972;
+const SpriggullFeatherBlue = 7918;
+const SpriggullFeatherGreen = 25446;
+const SpriggullFeatherPurple = 36248;
+const SpriggullFeatherRed = 18734;
+const SpriggullFletchingRed = 24072;
+const SpriggullFletchingBlue = 50608;
+const TomatoFullBurnt = 38742;
+const TomatoFullCooked = 38098;
+const TomatoFullRipe = 37454;
+const TomatoFullUnripe = 36810;
+const TomatoHalfBurnt = 57698;
+const TomatoHalfCooked = 57294;
+const TomatoHalfRipe = 56890;
+const TomatoHalfUnripe = 56486;
+
 export const getMaterialComponents = (speech: string): number[] => {
-  switch (speech) {
+  const source = speech.split(' ').reverse()[0];
+
+  switch (source) {
+    case 'babu':
+      return [BabuLegBone, BabuLegFullBurnt, BabuLegHalfBurnt, SoftFabricMediumRoll, SoftFabricMediumStrips];
+
+    case 'dais':
+      return [DaisMeatFullBurnt, DaisMeatHalfBurnt, SoftFabricMediumRoll, SoftFabricMediumStrips];
+
+    case 'earth':
+      return [CarsiIngot, CopperIngot, EvinonSteelIngot, GoldIngot, IronIngot, OrchiIngot, RedIronIngot, SilverIngot];
+
+    case 'embers':
+      return [Coal];
+
+    case 'gems':
+      return [CrystalGemBlue, CrystalShardBlue];
+
+    case 'gotera':
+      return [GoteraSeedlingOrb, RedwoodGoteraCore];
+
+    case 'fungi':
+      return [
+        MushroomBrownFullBurnt,
+        MushroomBrownHalfBurnt,
+        MushroomCaveLargeHalfBurnt,
+        MushroomCaveSmallFullBurnt,
+        MushroomCaveSmallHalfBurnt,
+        MushroomGlowingFullBurnt,
+        MushroomGlowingHalfBurnt,
+        MushroomRedFullBurnt,
+        MushroomRedHalfBurnt
+      ];
+
+    case 'glass':
+      return [PotionMedium];
+
+    case 'produce':
+      return [
+        AppleCoreBurnt,
+        AppleFullBurnt,
+        AppleHalfBurnt,
+        BlueberryFullBurnt,
+        CarrotFullBurnt,
+        CarrotHalfBurnt,
+        CarrotLeaves,
+        EggplantFullBurnt,
+        EggplantHalfBurnt,
+        GarlicFullBurnt,
+        GarlicHalfBurnt,
+        GarlicLeaves,
+        GarlicRoots,
+        OnionFullBurnt,
+        OnionHalfBurnt,
+        OnionLeaves,
+        OnionRoots,
+        PotatoFullBurnt,
+        PotatoHalfBurnt,
+        PotatoSapling,
+        PumpkinFullBurnt,
+        PumpkinHalfBurnt,
+        PumpkinPieceBurnt,
+        TomatoFullBurnt,
+        TomatoHalfBurnt
+      ];
+
+    case 'pyre':
+      return [Dynamite, Firework];
+
+    case 'revenant':
+      return [PhantomGuard];
+
+    case 'salt':
+      return [Salt];
+
+    case 'spriggull':
+      return [
+        SpriggullDrumstickBone,
+        SpriggullDrumstickFullBurnt,
+        SpriggullDrumstickHalfBurnt,
+        SpriggullFeatherBlue,
+        SpriggullFeatherGreen,
+        SpriggullFeatherPurple,
+        SpriggullFeatherRed,
+        SpriggullFletchingBlue,
+        SpriggullFletchingRed
+      ];
+
+    case 'turabada':
+      return [ExplosiveSpike];
+
+    case 'wyrm':
+      return [SoftFabricMediumRoll, SoftFabricMediumStrips];
+
     default:
       return [];
   }
