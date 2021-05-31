8a9d7bc4e367ea248c33fe07b9d8739b115191ae
Marc Dingena <marc.dingena@gmail.com>
2021-05-31 09:44:10 +0800

    No need for incantations in prepared spells

--- src/ui/atoms/preparedSpellsAtom.ts
+++ src/ui/atoms/preparedSpellsAtom.ts
@@ -1,10 +1,8 @@
 import { atom } from 'jotai';
-import { Incantation } from './incantationsAtom';
 
 export type PreparedSpell = {
   name: string;
   verbalTrigger: string;
-  incantations: Incantation[];
 };
 
 export const preparedSpellsAtom = atom<PreparedSpell[]>([]);
