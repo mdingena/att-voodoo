2d24b040208ed05a0af79fc43ee077f4dfb8f0cd
Marc Dingena <marc.dingena@gmail.com>
2021-07-02 19:43:11 +0800

    Open a modal to confirm the upgrade

--- src/ui/components/UpgradeSpell/UpgradeSpell.tsx
+++ src/ui/components/UpgradeSpell/UpgradeSpell.tsx
@@ -1,4 +1,6 @@
-import { Spell, SpellUpgrades } from '@/atoms';
+import { useState } from 'react';
+import { Spell, SpellUpgrades, UpgradeConfig } from '@/atoms';
+import { UpgradeModal } from '../UpgradeModal';
 import styles from './UpgradeSpell.module.css';
 
 interface UpgradeSpellProps {
@@ -8,23 +10,41 @@ interface UpgradeSpellProps {
 }
 
 export const UpgradeSpell = ({ spell, upgrades, onClose }: UpgradeSpellProps): JSX.Element => {
+  const [selectedUpgrade, setSelectedUpgrade] = useState<UpgradeConfig | null>(null);
+  const selectedUpgradeKey =
+    (selectedUpgrade && Object.entries(spell.upgrades).find(([, { name }]) => name === selectedUpgrade.name)?.[0]) ??
+    '';
+
+  const closeModal = () => setSelectedUpgrade(null);
+
+  const selectUpgrade = (upgradeConfig: UpgradeConfig) => () => setSelectedUpgrade(upgradeConfig);
+
   return (
-    <div className={styles.root}>
-      <div className={styles.header}>{spell.name}</div>
-      <div className={styles.upgrades}>
-        {Object.entries(spell.upgrades).map(([upgradeKey, upgradeConfig]) => (
-          <button key={upgradeConfig.name} className={styles.upgrade} onClick={() => undefined}>
-            <div className={styles.title}>
-              <span className={styles.level}>{upgrades[upgradeKey] ?? 0}</span>
-              <span className={styles.name}>{upgradeConfig.name}</span>
-            </div>
-            <div className={styles.description}>{upgradeConfig.description}</div>
-          </button>
-        ))}
+    <>
+      <div className={styles.root}>
+        <div className={styles.header}>{spell.name}</div>
+        <div className={styles.upgrades}>
+          {Object.entries(spell.upgrades).map(([upgradeKey, upgradeConfig]) => (
+            <button key={upgradeConfig.name} className={styles.upgrade} onClick={selectUpgrade(upgradeConfig)}>
+              <div className={styles.title}>
+                <span className={styles.level}>{upgrades[upgradeKey] ?? 0}</span>
+                <span className={styles.name}>{upgradeConfig.name}</span>
+              </div>
+              <div className={styles.description}>{upgradeConfig.description}</div>
+            </button>
+          ))}
+        </div>
+        <div className={styles.close}>
+          <button onClick={onClose}>&lt; Back</button>
+        </div>
       </div>
-      <div className={styles.close}>
-        <button onClick={onClose}>&lt; Back</button>
-      </div>
-    </div>
+      {selectedUpgrade && (
+        <UpgradeModal
+          upgradeConfig={selectedUpgrade}
+          currentLevel={upgrades[selectedUpgradeKey] ?? 0}
+          onClose={closeModal}
+        />
+      )}
+    </>
   );
 };
