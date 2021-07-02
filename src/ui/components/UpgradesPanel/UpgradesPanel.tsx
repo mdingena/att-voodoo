35a4792f9536b8d56f63aedc7950f41546f57a65
Marc Dingena <marc.dingena@gmail.com>
2021-07-02 14:06:06 +0800

    Update Experience component with stateful data

--- src/ui/components/UpgradesPanel/UpgradesPanel.tsx
+++ src/ui/components/UpgradesPanel/UpgradesPanel.tsx
@@ -1,26 +1,36 @@
 import { useState } from 'react';
 import { useAtom } from 'jotai';
-import { Panel, panelAtom, School } from '@/atoms';
+import { experienceAtom, Panel, panelAtom, School } from '@/atoms';
 import { Experience } from '../Experience';
 import { SpellFinder } from '../SpellFinder';
 import styles from './UpgradesPanel.module.css';
 
 export const UpgradesPanel = (): JSX.Element => {
   const [selectedSchool, setSelectedSchool] = useState<School | null>(null);
+  const [experience] = useAtom(experienceAtom);
   const [panel, setPanel] = useAtom(panelAtom);
 
   const closePanel = () => {
     setPanel(Panel.None);
   };
 
-  const closeSpellFinder = () => {
-    setSelectedSchool(null);
-  };
+  const closeSpellFinder = () => setSelectedSchool(null);
 
   const selectAbjuration = () => setSelectedSchool('abjuration');
   const selectConjuration = () => setSelectedSchool('conjuration');
   const selectEvocation = () => setSelectedSchool('evocation');
   const selectTransmutation = () => setSelectedSchool('transmutation');
+
+  const {
+    abjurationXpSpent,
+    abjurationXpTotal,
+    conjurationXpSpent,
+    conjurationXpTotal,
+    evocationXpSpent,
+    evocationXpTotal,
+    transmutationXpSpent,
+    transmutationXpTotal
+  } = experience;
 
   const isOpen = panel === Panel.Upgrades;
 
@@ -37,10 +47,30 @@ export const UpgradesPanel = (): JSX.Element => {
               </p>
               <p>Use the upgrade tracks below to find spells to upgrade.</p>
             </div>
-            <Experience onClick={selectAbjuration} school='Abjuration' total={0} spent={0} />
-            <Experience onClick={selectConjuration} school='Conjuration' total={13150} spent={13000} />
-            <Experience onClick={selectEvocation} school='Evocation' total={24580} spent={23000} />
-            <Experience onClick={selectTransmutation} school='Transmutation' total={7750} spent={4000} />
+            <Experience
+              onClick={selectAbjuration}
+              school='Abjuration'
+              total={abjurationXpTotal}
+              spent={abjurationXpSpent}
+            />
+            <Experience
+              onClick={selectConjuration}
+              school='Conjuration'
+              total={conjurationXpTotal}
+              spent={conjurationXpSpent}
+            />
+            <Experience
+              onClick={selectEvocation}
+              school='Evocation'
+              total={evocationXpTotal}
+              spent={evocationXpSpent}
+            />
+            <Experience
+              onClick={selectTransmutation}
+              school='Transmutation'
+              total={transmutationXpTotal}
+              spent={transmutationXpSpent}
+            />
           </div>
         </div>
         <div className={styles.close}>
