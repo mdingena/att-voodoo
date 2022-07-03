6365825aca89bf3f9f1c7c76fd726ce0104f5a38
Marc Dingena <marc.dingena@gmail.com>
2022-07-03 21:39:04 +0800

    Add studying hints

--- src/ui/components/Dock/Dock.tsx
+++ src/ui/components/Dock/Dock.tsx
@@ -1,5 +1,5 @@
 import { useAtom } from 'jotai';
-import { speechModeAtom, SpeechMode, incantationsAtom } from '@/atoms';
+import { speechModeAtom, SpeechMode, incantationsAtom, studyingAtom, StudyFeedback } from '@/atoms';
 import styles from './Dock.module.css';
 
 enum Slot {
@@ -18,21 +18,47 @@ interface DockProps {
 export const Dock = ({ slot, verbalComponentHint, materialComponentHint }: DockProps): JSX.Element => {
   const [speechMode] = useAtom(speechModeAtom);
   const [incantations] = useAtom(incantationsAtom);
+  const [studying] = useAtom(studyingAtom);
 
   const isHinting = verbalComponentHint && materialComponentHint;
   const isIncanting = speechMode === SpeechMode.Incanting;
   const isActiveDock = isIncanting && slot === incantations.length;
-  const incantingStyle = incantations[slot] ? styles.filled : isHinting ? styles.disabled : styles.empty;
-  const dockStyle = isIncanting ? (isActiveDock ? styles.active : incantingStyle) : styles.disabled;
 
   const dockIncantation = incantations[slot] ?? null;
   const verbalComponent = dockIncantation?.[0].toUpperCase();
   const materialComponent = dockIncantation?.[1];
+  const studyFeedback = dockIncantation?.[2];
+
+  const hintingStyle = isHinting ? styles.disabled : styles.empty;
+  const submittedStyle = incantations[slot] ? styles.filled : hintingStyle;
+  const incantingStyle = isActiveDock ? styles.active : submittedStyle;
+  const feedbackStyle =
+    typeof studyFeedback === 'undefined'
+      ? styles.disabled
+      : studyFeedback === StudyFeedback.Match
+      ? styles.feedbackMatch
+      : studyFeedback === StudyFeedback.Mismatch
+      ? styles.feedbackMismatch
+      : styles.feedbackPartial;
+  const disabledStyle = studying ? feedbackStyle : styles.disabled;
+
+  const dockStyle = isIncanting ? incantingStyle : disabledStyle;
+
+  const dockIcon =
+    studying && !isIncanting
+      ? typeof studyFeedback === 'undefined'
+        ? Slot[slot]
+        : studyFeedback === StudyFeedback.Match
+        ? 'Ξ'
+        : studyFeedback === StudyFeedback.Mismatch
+        ? 'Χ'
+        : 'θ'
+      : Slot[slot];
 
   return (
     <div className={dockStyle}>
       <div className={styles.dock}>
-        <span className={styles.slot}>{Slot[slot]}</span>
+        <span className={styles.slot}>{dockIcon}</span>
       </div>
       <div className={styles.description}>
         {dockIncantation ? (
